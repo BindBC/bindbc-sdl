d305095a4102d86abb881561d7fd1676d507111d
Michael Parker <aldacron@gmail.com>
2018-03-12 15:53:47 +0900

    Add licensing info.

--- source/bindbc/sdl/bind/sdlaudio.d
+++ source/bindbc/sdl/bind/sdlaudio.d
@@ -1,3 +1,9 @@
+
+//          Copyright Michael D. Parker 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 module bindbc.sdl.bind.sdlaudio;
 
 import bindbc.sdl.config;
--- source/bindbc/sdl/bind/sdlkeyboard.d
+++ source/bindbc/sdl/bind/sdlkeyboard.d
@@ -1,3 +1,9 @@
+
+//          Copyright Michael D. Parker 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 module bindbc.sdl.bind.sdlkeyboard;
 
 import bindbc.sdl.bind.sdlkeycode : SDL_Keycode, SDL_Keymod;

069405dcc53a7b84ace605a30dd98e3ee0a6b0ea
Mike Parker <aldacron@gmail.com>
2020-05-20 18:56:54 +0900

    Add support for the SDL_Thread API

--- source/bindbc/sdl/package.d
+++ source/bindbc/sdl/package.d
@@ -17,3 +17,16 @@ static if(bindSDLMixer) public import bindbc.sdl.mixer;
 static if(bindSDLNet) public import bindbc.sdl.net;
 static if(bindSDLTTF) public import bindbc.sdl.ttf;
 
+/*
+ Putting this here allows me to match the SDL_thread.h interface without any
+ internal conflicts (which cause a runtime crash when the loader tries to
+ load the SDL_CreateThread* functions into these aliases--actual functions--
+ rather than the funcion pointers).
+*/
+version(Windows) {
+    alias SDL_CreateThread = SDL_CreateThreadImpl;
+
+    static if(sdlSupport >= SDLSupport.sdl209) {
+        alias SDL_CreateThreadWithStackSize = SDL_CreateThreadWithStackSizeImpl;
+    }
+}

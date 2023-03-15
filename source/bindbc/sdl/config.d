bfe13c42c8b9e44abfd9a57bb4d6b3e9ebdedfa7
IchorDev <ichordev@noreply.git.sleeping.town>
2023-03-15 16:07:01 +1000

    Removed Import C dependency

--- source/bindbc/sdl/config.d
+++ source/bindbc/sdl/config.d
@@ -121,39 +121,31 @@ enum bindSDLTTF = (){
 	else return false;
 }();
 
-//NOTE: everything below here may be moved to another library in the future
+//NOTE: everything below here will be moved to BindBC-Common in the future
 
-static if(__VERSION__ >= 2101L){ //2.101+ supports Import C with #include
-	public import bindbc.sdl.ctypes: c_long, c_ulong;
+version(WebAssembly){
+	alias c_long  = long;
+	alias c_ulong = ulong;
 	
-	//these `c_` type aliases are here because these types use #define on some platforms
-	private import bindbc.sdl.ctypes: c_wchar_t, c_va_list;
-	alias wchar_t = c_wchar_t;
-	alias va_list = c_va_list;
+	alias va_list = void*;
 }else{
-	version(WebAssembly){
-		alias c_long  = long;
-		alias c_ulong = ulong;
-		
-		alias va_list = void*;
-	}else{
-		public import core.stdc.config: c_long, c_ulong;
-		public import core.stdc.stdarg: va_list;
-	}
-	
-	static if((){
-		version(Posix)     return true;
-		else version(WASI) return true;
-		else return false;
-	}()){
-		alias wchar_t = dchar;
-	}else static if((){
-		version (Windows) return true;
-		else return false;
-	}()){
-		alias wchar_t = wchar;
-	}else static assert(0, "`sizeof(wchar_t)` is not known on this platform. Please add it to bindbc/sdl/config.d, or update your compiler to a version based on dmd 2.101 or higher");
+	public import core.stdc.config: c_long, c_ulong;
+	public import core.stdc.stdarg: va_list;
 }
+
+static if((){
+	version(Posix)     return true;
+	else version(WASI) return true;
+	else return false;
+}()){
+	alias wchar_t = dchar;
+}else static if((){
+	version(Windows)    return true;
+	else version(WinRT) return true;
+	else return false;
+}()){
+	alias wchar_t = wchar;
+}else static assert(0, "`sizeof(wchar_t)` is not known on this platform. Please add it to bindbc/sdl/config.d");
 
 deprecated("This template will be moved to another library in the future") enum expandEnum(EnumType, string fqnEnumType = EnumType.stringof) = () nothrow pure @safe{
 	string expandEnum;

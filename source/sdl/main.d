f0d3bfcc2f8a0cbc92433f0f9b7c56628b23c289
IchorDev <15670465+ichordev@users.noreply.github.com>
2024-01-18 15:21:47 +0700

    Happy 2024!

--- source/sdl/main.d
+++ source/sdl/main.d
@@ -1,5 +1,5 @@
 /+
-+                Copyright 2023 Aya Partridge
++            Copyright 2023 – 2024 Aya Partridge
 + Distributed under the Boost Software License, Version 1.0.
 +     (See accompanying file LICENSE_1_0.txt or copy at
 +           http://www.boost.org/LICENSE_1_0.txt)
--- source/sdl_net.d
+++ source/sdl_net.d
@@ -1,5 +1,5 @@
 /+
-+            Copyright 2022 – 2023 Aya Partridge
++            Copyright 2022 – 2024 Aya Partridge
 +          Copyright 2018 - 2022 Michael D. Parker
 + Distributed under the Boost Software License, Version 1.0.
 +     (See accompanying file LICENSE_1_0.txt or copy at
